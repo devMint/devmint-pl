0b7657ba04b0d77e0545343e1dc75ff12de6b512
Grzegorz Gajda <grzegorz.gajda@devmint.pl>
2025-07-02 13:02:20 +0200

    chore: cache for actions

--- packages/devmint-pl/astro.config.mjs
+++ packages/devmint-pl/astro.config.mjs
@@ -1,7 +1,6 @@
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwindcss from '@tailwindcss/vite'
-// @ts-check
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
