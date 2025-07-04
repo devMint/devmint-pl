4fe3c2bef2a5da8ee7b0ffc1a4ecf42931461f30
Grzegorz Gajda <grzegorz.gajda@devmint.pl>
2025-07-04 13:11:28 +0200

    feat: favicon + better dimensions for post pictures, optimized font loadings

--- packages/devmint-pl/astro.config.mjs
+++ packages/devmint-pl/astro.config.mjs
@@ -2,10 +2,26 @@ import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'astro/config'
+import favicons from 'astro-favicons'
 
 export default defineConfig({
   site: 'https://devmint.pl',
-  integrations: [mdx(), sitemap()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    favicons({
+      name: 'devMint',
+      short_name: 'devMint',
+      background: '#28a282',
+      input: {
+        appleIcon: 'public/logo.png',
+        appleStartup: 'public/logo.png',
+        windows: 'public/logo.png',
+        android: 'public/logo.png',
+        favicon: 'public/favicon.svg',
+      },
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
